df6c31dc4634eb384c2ec837304f0a90f89897cf
Johanna Thorsen <39465291+Johannathorsen@users.noreply.github.com>
2020-11-20 20:21:28 +0000

    Update MessageBox.js
    Answers to questions

--- MessageBox.js
+++ MessageBox.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 import './MessageBox.css';
 
+/*
+* I think the return function displays the actual message saying whether or not there is an error?
+* 
+* You can see what in App.js what the "props" parameter means.
+* props.message is the response coming back from the backend. It could be any information.
+* props.error is a boolean (i.e. it can only have the value true or false), saying that there is an error or not.
+* If props.error is true, it'll add a CSS class to the element, called Error. You can see in the file "MessageBox.css" that it'll change the color of the message.
+*/
+
 function MessageBox(props) {
-    const classes = `Message${props.error ? ' Error' : ''}`; // I think the return function displays the actual message saying whether or not there is an error?
+    const classes = `Message${props.error ? ' Error' : ''}`;
     return <div className={classes}>{props.message}</div>;
 }
 
